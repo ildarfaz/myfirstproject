0028a200f2af89705ad2b7382c6ae2f4303c97d7
ildarfaz <ildarfazylbekov@gmail.com>
2021-10-08 10:45:58 +0500

    changeInput

--- src/inputs/Input.tsx
+++ src/inputs/Input.tsx
@@ -6,7 +6,7 @@ export const Input: React.FC<IProps> = ({ onChange }) => {
   return (
     <div className="header_input">
       <input
-        type="text"
+        type="search"
         placeholder="Search for a contact"
         onChange={(event) => onChange(event.target.value)}
       />
