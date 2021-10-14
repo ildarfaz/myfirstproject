69d7109f73fa3217cc3223c2b2b4360e651652d5
ildarfaz <ildarfazylbekov@gmail.com>
2021-10-14 18:56:01 +0500

    changeAppUsersTable

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React, { FC } from "react";
+import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Main } from "./components/main/Main";
-const App: FC = () => {
+const App: React.FC = () => {
   return (
     <Router>
       <div className="App">
