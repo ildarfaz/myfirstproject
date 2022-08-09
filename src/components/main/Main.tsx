c3203a9a3a3480d04faaf9ad61cf111bd012aaa9
ildarfaz <ildarfazylbekov@gmail.com>
2022-08-09 10:32:28 +0500

    обновился до react 18

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Input } from "../../inputs/Input";
 import "./Main.scss";
-import { Switch, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 import { Contacts } from "../contacts/Contacts";
 import { Dashboard } from "../dashboard/Dashboard";
 import { SideBar } from "../sidebar/SideBar";
@@ -10,23 +10,17 @@ export const Main = () => {
   return (
     <div className="wrapper">
       <SideBar />
-  
+
       <div className="header_content">
         <div className="header">
           <Input onChange={setInputValue} />
         </div>
         <hr />
-        <Switch>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/contacts">
-            <Contacts inputValue={inputValue} />
-          </Route>
-          <Route path="/">
-            <Contacts inputValue={inputValue} />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/contacts" element={<Contacts inputValue={inputValue} />} />
+          <Route path="/" element={<Contacts inputValue={inputValue} />} />
+        </Routes>
       </div>
     </div>
   );
