4012618aba87946e5c9ac1bcabfe50884849a9c0
ildarfaz <ildarfazylbekov@gmail.com>
2023-01-14 13:43:04 +0500

    del type

--- src/store/Store.tsx
+++ src/store/Store.tsx
@@ -1,8 +1,5 @@
 import {  configureStore,  } from '@reduxjs/toolkit'
 import { rootReducer } from "./reducers/rootReducer";
-
-
-export type RootState = ReturnType<typeof rootReducer>
 export const store = configureStore({
   reducer: rootReducer,
   middleware: getDefaultMiddleware => getDefaultMiddleware(),
